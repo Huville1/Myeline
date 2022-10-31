356b9fee337942478d4c218b8f07a8c58c1ba976
Eric <ehu39@gatech.edu>
2022-10-31 18:49:02 -0400

    testing while loop with electrodes

--- Pi/OLED/midline.py
+++ Pi/OLED/midline.py
@@ -42,18 +42,28 @@ draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
 draw.line((0,16,oled.width,16), fill = 255, width= 1)
 
 #E1
-draw.text((0, 0), "E1", font=font, fill=255)
-draw.rectangle ((0,17,15,oled.height-1),outline = 255, fill =0);
+enum = 1
+xSpace = 128/2(*enum)
+i=0
+while i < enum:
+    draw.text((0+(i*xSpace),0), "E" + str(i+1), font = font, fill = 255)
+    i+=1
 
-#mini electrode dots -> need to make this into a loop
-draw.rectangle ((6,21,10,25),outline = 255, fill = 0)
-draw.rectangle ((6,28,10,32),outline = 255, fill = 0)
-draw.rectangle ((6,35,10,39),outline = 255, fill = 0)
-draw.rectangle ((6,42,10,46),outline = 255, fill = 0)
-draw.rectangle ((6,49,10,53),outline = 255, fill = 0)
-draw.rectangle ((6,56,10,60),outline = 255, fill = 0)
+
+
+
+# draw.text((0, 0), "E1", font=font, fill=255)
+# draw.rectangle ((0,17,15,oled.height-1),outline = 255, fill =0);
+
+# #mini electrode dots -> need to make this into a loop
+# draw.rectangle ((6,21,10,25),outline = 255, fill = 0)
+# draw.rectangle ((6,28,10,32),outline = 255, fill = 0)
+# draw.rectangle ((6,35,10,39),outline = 255, fill = 0)
+# draw.rectangle ((6,42,10,46),outline = 255, fill = 0)
+# draw.rectangle ((6,49,10,53),outline = 255, fill = 0)
+# draw.rectangle ((6,56,10,60),outline = 255, fill = 0)
 #electrode name splitter
-draw.line((16,0,16,16), fill = 255, width= 1)
+#
 
 # lnTh = 10
 # # electrode design
