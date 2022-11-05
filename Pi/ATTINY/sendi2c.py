bda5643982dd1079616114ee3a13ceebc6c5b7ae
Eric <ehu39@gatech.edu>
2022-11-05 17:46:09 -0500

    messed with code

--- Pi/ATTINY/sendi2c.py
+++ Pi/ATTINY/sendi2c.py
@@ -1,5 +1,5 @@
 import sys
-import smbus2 as smbus
+import smbus2 as SMBus
 import time
 
 I2C_SLAVE_ADDRESS = 11
@@ -16,17 +16,17 @@ def ConvertStringsToBytes(src):
 
 def main(args):
     #create the I2C bus
-    I2Cbus = smbus.SMBus(1)
-    with smbus.SMBUS(1) as I2Cbus:
-        while True:
-            if (i == 6):
-                i= i-6
-            message = lights[i]
-            #bytestoSend = ConvertStringsToBytes(message)
-            print(message)
-            I2Cbus.write_byte_data(80, 0, message)
-            time.sleep(1)
-            i+=1
+    I2Cbus = SMBus(1)
+    # with smbus.SMBUS(1) as I2Cbus:
+    while True:
+        if (i == 6):
+            i= i-6
+        message = lights[i]
+        #bytestoSend = ConvertStringsToBytes(message)
+        print(message)
+        I2Cbus.write_byte_data(80, 0, message)
+        time.sleep(1)
+        i+=1
             
     return 0
 if __name__ == '__main__':
