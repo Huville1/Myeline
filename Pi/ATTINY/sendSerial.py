fc168f0ca66db88d1cd5b32d19d91ae263ba6d0f
Eric <ehu39@gatech.edu>
2022-11-09 13:36:24 -0500

    printline

--- Pi/ATTINY/sendSerial.py
+++ Pi/ATTINY/sendSerial.py
@@ -1,11 +1,16 @@
 import serial 
-
+import numpy as np
+data = [] # all data collected from pins on module
+i = 0 # pin number
+temp = 0
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.reset_input_buffer()
     while True:
         if ser.in_waiting > 0:
-            byte = ser.read()
-            if byte != b'':
-                ints = int.from_bytes(byte,byteorder = 'big')
-                print(ints)
+            byte = ser.readline().decode('utf-8').rstrip()
+            print(byte)
+            # print( yte)
+            # if byte != b'':
+            #     ints = int.from_bytes(byte,byteorder = 'big')
+            #     print(ints)
