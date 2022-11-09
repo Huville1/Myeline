c3962bed1ae46d364bec60da5ff0090a2656062c
Eric <ehu39@gatech.edu>
2022-11-09 13:46:32 -0500

    plotting

--- Pi/ATTINY/sendSerial.py
+++ Pi/ATTINY/sendSerial.py
@@ -1,15 +1,19 @@
 import serial 
 import numpy as np
+from matplotlib import pyplot as plt
 data = [] # all data collected from pins on module
 i = 0 # pin number
 temp = 0
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.reset_input_buffer()
-    while True:
+    while 5000:
         if ser.in_waiting > 0:
             byte = ser.readline().decode('utf-8').rstrip()
-            print(byte)
+            data.append(byte)
+        i+=1
+    plt.plot(data)
+    plt.show()
             # print( yte)
             # if byte != b'':
             #     ints = int.from_bytes(byte,byteorder = 'big')
