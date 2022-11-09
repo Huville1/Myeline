4d9fc357aafd0a0166ca2f0e3433e7b0db00768f
Eric <ehu39@gatech.edu>
2022-11-09 13:50:29 -0500

    reset

--- Pi/ATTINY/sendSerial.py
+++ Pi/ATTINY/sendSerial.py
@@ -1,19 +1,15 @@
 import serial 
 import numpy as np
-import matplotlib.pyplot as plt
 data = [] # all data collected from pins on module
 i = 0 # pin number
 temp = 0
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.reset_input_buffer()
-    while 5000:
+    while True:
         if ser.in_waiting > 0:
             byte = ser.readline().decode('utf-8').rstrip()
-            data.append(byte)
-        i+=1
-    plt.plot(data)
-    plt.show()
+            print(byte)
             # print( yte)
             # if byte != b'':
             #     ints = int.from_bytes(byte,byteorder = 'big')
