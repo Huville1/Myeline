65f247af722c2e2efbd1477d26dfb8a18fc34d5c
Eric <ehu39@gatech.edu>
2022-11-20 23:07:19 -0500

    serial changed

--- Pi/ATTINY/sendSerial.py
+++ Pi/ATTINY/sendSerial.py
@@ -10,7 +10,7 @@ i = 0 # pin number
 temp = 0
 def receive (secs:float) -> list: # takes in how long to read, output data  
     data = []
-    ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 # ser.open()
     if ser.is_open == True:
         print("\nAll right, serial port now open. Configuration:\n")
