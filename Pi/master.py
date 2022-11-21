103c48ff7390122380122a9cc6b47e5721564c72
Eric <ehu39@gatech.edu>
2022-11-20 23:05:56 -0500

    masterpush

--- Pi/master.py
+++ Pi/master.py
@@ -1,7 +1,7 @@
 import numpy as np
 # from OLED.display import display
 from Screen.display import display
-# from ATTINY.sendi2c import elecSend
+from ATTINY.sendi2c import elecSend
 from ATTINY.sendSerial import receive
 import time
 from scipy.signal import find_peaks # need to import scipy
@@ -14,63 +14,69 @@ tempD = []
 i = 0
 addresses =[4]
 #need to find how many electrodes are attached
-# for address in addresses:
-#     while i < 6: # ----> looping through all electrode pins
-#         #send signal to module
-#         elecSend(address, i)
-#         time.sleep(1.667)
+for address in addresses:
+    while i < 6: # ----> looping through all electrode pins
+        #send signal to module
+        elecSend(address, i)
+        time.sleep(1.667)
 #         # receive 
-#         tempD = receive(5) #receiving data for 5 seconds
-#         allData.append(tempD)
-#         tempD =[]
-#         i+=1
+        tempD = receive(5) #receiving data for 5 seconds
+        allData.append(tempD)
+        tempD =[]
+        i+=1
 
 #insert signal processing info
 #variables
-j = 0
+# j = 0
 # data = receive(5)
 # print(len(data))
-filename = open('SpinalCordSampleData.csv', 'r')
+# filename = open('SpinalCordSampleData.csv', 'r')
  
-# creating dictreader object
-file = csv.DictReader(filename)
+# # creating dictreader object
+# file = csv.DictReader(filename)
  
-# creating empty lists
-left = []
-right = []
-middle = []
+# # creating empty lists
+# left = []
+# right = []
+# middle = []
  
-# iterating over each row and append
-# values to empty list
-i = 0
-for col in file:
-    if(i < 531):
-        left.append(int(col['left']))
-        right.append(int(col['right']))
-        middle.append(int(col['midline']))
-    else:
-        break
-    i+=1
+# # iterating over each row and append
+# # values to empty list
+# i = 0
+# for col in file:
+#     if(i < 531):
+#         left.append(int(col['left']))
+#         right.append(int(col['right']))
+#         middle.append(int(col['midline']))
+#     else:
+#         break
+#     i+=1
 
 # print(len(left))
 # print(left)
 # print(len(right))
 
-allData = []
+# allData = []
 # allData.append(left)
 # allData.append(left)
 # allData.append(middle)
 
 # allData.append(right)
 # allData.append(right)
-i = 0
-while i < 6:
-    print("start stim")
-    tempD = receive(5)
-    allData.append(tempD)
-    print("switch location")
-    time.sleep(5)
-    i+=1
+
+
+
+
+
+################# NEED THIS PART UNDER ###############
+# j = 0
+# while j < 6:
+#     print("start stim")
+#     tempD = receive(5)
+#     allData.append(tempD)
+#     print("switch location")
+#     time.sleep(5)
+#     i+=1
 
 print("data is appended")
 averages = [] # 6*len(addresses long)
